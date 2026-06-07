8707abd696218ceeb3a563cde6619968a0c3d473
MaiconDante <maicondante@hotmail.com>
2026-06-07 17:28:33 -0300

    feat: Adiciona a marcação de horas rotacionado.

--- main.py
+++ main.py
@@ -21,25 +21,48 @@ class AnalogClock(QWidget):
         # Fundo
         painter.fillRect(self.rect(), QColor("#0F172A"))
 
-        # Centraliza coordenadas
+        # Move origem para centro
         painter.translate(self.width() / 2, self.height() / 2)
 
         # Tamanho do relógio
         radius = min(self.width(), self.height()) / 2 - 40
 
-        # Borda do relógio
-        pen = QPen(QColor("#E2E8F0"))
-        pen.setWidth(4)
+        # =========================
+        # Círculo externo
+        # =========================
 
-        painter.setPen(pen)
+        border_pen = QPen(QColor("#E2E8F0"))
+        border_pen.setWidth(4)
 
-        # Desenha círculo principal
+        painter.setPen(border_pen)
+
         painter.drawEllipse(
             int(-radius),
             int(-radius),
             int(radius * 2),
             int(radius * 2)
         )
+
+        # =========================
+        # Marcações das horas
+        # =========================
+
+        hour_pen = QPen(QColor("#F8FAFC"))
+        hour_pen.setWidth(5)
+
+        painter.setPen(hour_pen)
+
+        for _ in range(12):
+
+            painter.drawLine(
+                0,
+                int(-radius + 20),
+                0,
+                int(-radius + 50)
+            )
+
+            # Rotaciona 30 graus
+            painter.rotate(30)
 
 
 app = QApplication(sys.argv)
